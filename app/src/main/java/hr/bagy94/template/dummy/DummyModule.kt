a7bc41dab4ed59fee2bdc1c173402a53c0ac269a
davor bagaric <dev.bagy94@gmail.com>
2019-09-19 00:47:23 +0200

    clear

--- app/src/main/java/hr/bagy94/template/dummy/DummyModule.kt
+++
@@ -1,8 +0,0 @@
-package hr.bagy94.template.dummy
-
-import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val dummyModule = module {
-    viewModel { DummyViewModel() }
-}
--- app/src/main/java/hr/bagy94/template/dummy/DummyViewModel.kt
+++
@@ -1,6 +0,0 @@
-package hr.bagy94.template.dummy
-
-import hr.bagy94.base_android.viewmodel.BaseViewModel
-
-class DummyViewModel : BaseViewModel() {
-}
